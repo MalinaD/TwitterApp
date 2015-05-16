f25cf033ff4483b5be73defba05bb11e673847d6
MalinaD <malina.demirova@gmail.com>
2015-05-16 19:47:27 +0300

    Logic changed. Login/Registration changed

---
+++ Twitter.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Twitter.Models;
+namespace Twitter.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
